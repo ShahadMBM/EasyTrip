c33805bfe4c978bf960c082e64a214e3d0ecc942
Shahad Benmohi <96819901+ShahadMBM@users.noreply.github.com>
2022-02-23 11:18:20 +0300

    ..

---
+++ EasyTrip/AvaliableCarCell.swift
@@ -0,0 +1,23 @@
+//
+//  AvaliableCarCell.swift
+//  EasyTrip
+//
+//  Created by Shahad on 22/07/1443 AH.
+//
+
+import UIKit
+
+class AvaliableCarCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
