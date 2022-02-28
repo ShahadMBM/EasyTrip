dc2db6496efde736990ca1331c1953e3292c5181
shahadq <shahad.mq20@gmail.com>
2022-02-28 10:32:58 +0300

    ..

---
+++ EasyTrip/AVCarsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  AVCarsCollectionViewCell.swift
+//  EasyTrip
+//
+//  Created by Shahad on 27/07/1443 AH.
+//
+
+import UIKit
+
+class AVCarsCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ EasyTrip/AVCarsVC.swift
@@ -0,0 +1,29 @@
+//
+//  AVCarsVC.swift
+//  EasyTrip
+//
+//  Created by Shahad on 27/07/1443 AH.
+//
+
+import UIKit
+
+class AVCarsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
