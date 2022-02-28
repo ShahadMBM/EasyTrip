6970f5d27c191f596715687339e20cea88d8e157
Shahad Benmohi <96819901+ShahadMBM@users.noreply.github.com>
2022-02-28 10:12:34 +0300

    ..

---
+++ EasyTrip/offerCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  offerCollectionViewCell.swift
+//  EasyTrip
+//
+//  Created by Shahad on 27/07/1443 AH.
+//
+
+import UIKit
+
+class offerCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var advs: UILabel!
+    @IBOutlet weak var cobon: UILabel!
+    
+    @IBOutlet weak var copyBtn: UIButton!
+    
+    
+    func setupCell (photo :UIImage, ad: String , co : String){
+       
+        img.image = photo
+        advs.text = ad
+        cobon.text = ad
+
+    }
+
+}
---
+++ EasyTrip/offerVC.swift
@@ -0,0 +1,56 @@
+//
+//  offerVC.swift
+//  EasyTrip
+//
+//  Created by Shahad on 27/07/1443 AH.
+//
+
+import UIKit
+
+class offerVC: UIViewController ,UICollectionViewDataSource, UICollectionViewDelegate , UICollectionViewDelegateFlowLayout{
+    
+    var arrOffer = [offer(photo:UIImage(named:"Uber")!, adv: "Invite a friend and get 14% offer", cobon: "#FRI14") , offer(photo:UIImage(named: "Careem")!, adv: "Invite a friend and get 17% offer", cobon: "#FRI17")]
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    //    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arrOffer.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "offerCell", for: indexPath) as! offerCollectionViewCell
+        let offer = arrOffer[indexPath.row]
+        cell.layer.cornerRadius = 20
+        cell.setupCell(photo:offer.photo, ad: offer.adv, co: offer.cobon)
+        return cell
+    
+}
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 78)
+    }
+
+    
+
+}
+
+struct offer {
+
+var photo : UIImage
+var adv: String
+var cobon : String
+  
+}
+
+
+
