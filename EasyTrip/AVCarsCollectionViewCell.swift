da728042ad0fe46c9a6fea61e55cdbfd344d0495
shahadq <shahad.mq20@gmail.com>
2022-03-01 11:17:34 +0300

    ..

--- EasyTrip/AVCarsCollectionViewCell.swift
+++ EasyTrip/AVCarsCollectionViewCell.swift
@@ -8,5 +8,22 @@
 import UIKit
 
 class AVCarsCollectionViewCell: UICollectionViewCell {
+   
+    @IBOutlet weak var TaxiImg: UIImageView!
+    
+    @IBOutlet weak var TaxiName: UILabel!
+    @IBOutlet weak var TaxiPrice: UILabel!
+    @IBOutlet weak var TaxiNearby: UILabel!
+    
+    @IBOutlet weak var taxiApp: UIButton!
+    
+    func setupCell (TXimg :UIImage, TXname: String , TXprice: String, TXnearby : String){
+       
+        TaxiImg.image = TXimg
+        TaxiName.text = TXname
+        TaxiPrice.text = TXprice
+        TaxiNearby.text = TXnearby
+
+    }
     
 }
--- EasyTrip/AVCarsVC.swift
+++ EasyTrip/AVCarsVC.swift
@@ -7,23 +7,57 @@
 
 import UIKit
 
-class AVCarsVC: UIViewController {
+class AVCarsVC: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate , UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
 
+    
+    var taxiapps =
+    [taxiapp(taxiname: "Uber", taxiprice: "SAR 26.92", taxiNearby: "Cars Nearby: 3", taxiImage:UIImage(named:"image1")!) ,
+                   
+     taxiapp(taxiname: "Jeeny", taxiprice: "SAR 27.43", taxiNearby: "Cars Nearby: 2", taxiImage:UIImage(named:"image2")!) ,
+                   
+     taxiapp(taxiname: "Careem", taxiprice: "SAR 30.76", taxiNearby: "Cars Nearby: 3", taxiImage:UIImage(named:"image3")!) ,
+                    
+     taxiapp(taxiname: "Bolt", taxiprice: "SAR 29.23", taxiNearby: "Cars Nearby: 1", taxiImage:UIImage(named:"image4")!)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return taxiapps.count
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "carCell", for: indexPath) as! AVCarsCollectionViewCell
+        let taxi = taxiapps[indexPath.row]
+    
+        
+        cell.layer.cornerRadius = 20
+        
+  cell.setupCell(TXimg:taxi.taxiImage, TXname: taxi.taxiname, TXprice: taxi.taxiprice, TXnearby: taxi.taxiNearby)
+        return cell
+    }
+    
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 116)
+   }
+   
+
+ 
+
+}
+
+struct taxiapp {
+    var taxiname: String
+    var taxiprice: String
+    var taxiNearby: String
+    var taxiImage: UIImage
+    
 }
