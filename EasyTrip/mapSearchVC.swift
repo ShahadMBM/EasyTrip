24cb8008237cabb4a28a601efa846c0e50c5c40b
Shahad Benmohi <96819901+ShahadMBM@users.noreply.github.com>
2022-03-01 09:13:05 +0300

    ..

---
+++ EasyTrip/mapSearchVC.swift
@@ -0,0 +1,21 @@
+//
+//  mapSearchVC.swift
+//  EasyTrip
+//
+//  Created by Shahad on 27/07/1443 AH.
+//
+
+import UIKit
+import MapKit
+
+class mapSearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
